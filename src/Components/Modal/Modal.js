68302ef5ebee58da8872d3ca119c6959026853bc
MarianoRamborger <Marianoramborger@gmail.com>
2020-06-22 15:48:07 -0300

    New one

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import Modal from '@material-ui/Modal';
-import Backdrop from '@material-ui/Backdrop';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
 import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
 
 const useStyles = makeStyles((theme) => ({
