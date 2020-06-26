ee5ca6cd1d4179955baae6c098bdc99be220f8fe
MarianoRamborger <Marianoramborger@gmail.com>
2020-06-26 12:07:18 -0300

    pid changed

--- src/Components/LeftBody/NosotrosBody.js
+++ src/Components/LeftBody/NosotrosBody.js
@@ -9,7 +9,35 @@ const NosotrosBody = () => {
 
         <h2 className="nosotros-section-title"> Acá iría información sobre el criadero</h2>
 
-        <p className="nosotros-section-p"> Historia, información sobre la raza, fotos que quiera poner, cualquier cosa que considere relevante! </p>
+        <p className="nosotros-section-p"> 
+
+        Llegan a nuestro hogar como seres pequeños e indefensos, palpitando al miedo de estar solos. <br />
+
+        Buscan un lugar donde comer y dormir. De a poco se van apropiando de la casa. <br />
+
+        Resignamos cosas nuestras que se van destrozando con sus travesuras <br/>
+
+        Nos ganan la paciencia, la atención, los cuidados. <br />
+
+        Conocen nuestros gestos, nuestra voz, esperan por nuestros diarios regresos, esperan por caricias, por abrazos, y sus colas se agitan 
+        como muestra de alegria. <br />
+
+        Prestan sus años a nuestro lado; nos entregan su corazón, y roban el nuestro. <br />
+
+        Quedan sus huellas marcadas en el espacio y tiempo de nuestro sentimiento. <br />
+
+        Llenan espacios vacíos, completan nuestra vida. <br />
+
+        Reunen valor para defender lo nuestro, que sienten que ya también es de ellos. <br />
+
+        Pasan de mascotas, a fieles amigos, a compañeros inseparables., a glotones persiguiendo la comida más sabrosa. <br />
+
+        ¿Como no dejar que compartan nuestros días, si son motivo para seguir viviendo?
+
+        
+        
+        
+        </p>
 
 
         </div>
